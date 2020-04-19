92536f122f54c64f19ae336bdb3484d6be5ed531
Will Johnston <wijohnst@gmail.com>
2020-04-19 17:45:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import DistrictsMap from './districts_map.svg'
+import {SvgLoader, SvgProxy} from 'react-svgmt'
 
-function App() {
+
+
+export default function App() {
+  
+  const districtData = [{district: '#AL-01', color: 'lightpink'},{district: '#AL-02', color: 'lightgreen'}, {district: '#AL-03', color: 'lightpink'},
+  {district: '#AL-04', color: 'lightyellow'}, {district: '#AL-05', color: 'lightblue'}, {district: '#AL-06', color: 'lightgreen'}, {district: '#AL-07', color: 'lightpink'}];
+
+  console.log(districtData[1].district);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SvgLoader path={DistrictsMap}>
+        {districtData.map((info) =>(
+            <SvgProxy key={info.district} selector={info.district} fill={info.color} />
+        ))}
+      </SvgLoader>
     </div>
-  );
+  )
 }
 
-export default App;
+
